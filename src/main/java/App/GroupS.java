2ba61d5cf6900ca6f1a6ce94e00c6c88427a8eb2
bartmy <70316323+kuccu@users.noreply.github.com>
2021-12-16 23:19:03 +0100

    adding user to database

--- src/main/java/App/GroupS.java
+++ src/main/java/App/GroupS.java
@@ -2,11 +2,20 @@ package App;
 
 import App.App;
 
+import java.sql.SQLException;
+
+
 public class GroupS {
-    public static void main(String[] args) {
-        App appStart = new App();
-        while (App.isProgramOn()){
-            appStart.startProgram();
-        }
+    public static void main(String[] args) throws SQLException {
+
+        Database sql = new Database();
+        sql.testConnectToDatabase();
+        sql.addUserToDatabase("test2", "test2");
+
+//
+//        App appStart = new App();
+//        while (App.isProgramOn()){
+//            appStart.startProgram();
+//        }
     }
 }
