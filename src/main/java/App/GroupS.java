eeedf6b15a49c06423dd1063d3d345336ddbd635
bartmy <70316323+kuccu@users.noreply.github.com>
2022-01-09 20:56:30 +0100

    GRP-51 - confirmation of invites - done and working GRP-58 - test all - done

--- src/main/java/App/GroupS.java
+++ src/main/java/App/GroupS.java
@@ -2,20 +2,16 @@ package App;
 
 import App.LandingPage.Login;
 
-
 public class GroupS {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
 //        App appStart = new App();
 //        while (App.isProgramOn()){
 //            appStart.startProgram();
 //        }
 
-//
         Login login = new Login();
         login.testLogin("bartekm", "haslo");
 
-//        Login login = new Login();
-//        login.testLogin("bartekm", "haslo");
     }
 }
