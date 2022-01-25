96dda70faef021ea8310637c5124493625a62f79
bartmy <70316323+kuccu@users.noreply.github.com>
2022-01-25 22:02:22 +0100

    GRP-74 - warstwy, testy jednostkowe

--- src/main/java/io/github/bartmy/http/HelloService.java
+++ src/main/java/io/github/bartmy/http/HelloService.java
@@ -1,11 +1,36 @@
 package io.github.bartmy.http;
 
-import java.util.Optional;
+import io.github.bartmy.App.ToDo.Lang;
+import io.github.bartmy.App.ToDo.LangRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.ILoggerFactory;
 
+
+import java.util.Optional;
+@Slf4j
 public class HelloService {
     static final  String FALLBACK_NAME = "world";
+    static final Lang FALLBACK_LANG = new Lang(1L, "Hello","en");
 
-    String prepareGreeting(String name){
-        return "Hello " + Optional.ofNullable(name).orElse(FALLBACK_NAME) + "!";
+    private LangRepository repository;
+
+    HelloService(){
+        this(new LangRepository());
+    }
+    HelloService(LangRepository repository){
+        this.repository = repository;
+    }
+
+    String prepareGreeting(String name, String lang){
+        Long langId;
+        try {
+            langId = Optional.ofNullable(lang).map(Long::valueOf).orElse(FALLBACK_LANG.getId());
+        } catch (NumberFormatException e){
+            log.warn("Non-numeric language id used: " + lang);
+            langId = FALLBACK_LANG.getId();
+        }
+        var welcomeMsg = repository.findById(langId).orElse(FALLBACK_LANG).getWelcomeMsg();
+        var nameToWelcome = Optional.ofNullable(name).orElse(FALLBACK_NAME);
+        return welcomeMsg + " " + nameToWelcome + "!";
     }
 }
