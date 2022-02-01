e23137ba3572a135eabc045786dc8b94a4e61cd1
bartmy <70316323+kuccu@users.noreply.github.com>
2022-02-01 21:52:32 +0100

    ToDo  GRP-84 - Todo class, repository and servlet created and working

---
+++ src/main/java/io/github/bartmy/App/todo/TodoRepository.java
@@ -0,0 +1,32 @@
+package io.github.bartmy.App.todo;
+
+import io.github.bartmy.App.lang.Lang;
+import io.github.bartmy.App.util.HibernateUtil;
+
+import java.util.List;
+import java.util.Optional;
+
+public class TodoRepository {
+
+    List<Todo> findAll(){
+        var session = HibernateUtil.getSessionFactory().openSession();
+        var transaction = session.beginTransaction();
+
+        var result = session.createQuery("from Todo", Todo.class).list();
+
+        transaction.commit();
+        session.close();
+        return result;
+    }
+    Todo toggleTodo(Integer id){
+        var session = HibernateUtil.getSessionFactory().openSession();
+        var transaction = session.beginTransaction();
+
+        var result = session.get(Todo.class, id);
+        result.setDone(!result.isDone());
+
+        transaction.commit();
+        session.close();
+        return result;
+    }
+}
---
+++ src/main/resources/db/migration/V8__Add_todos_data.sql
@@ -0,0 +1,2 @@
+insert into todos (text, done) values ('Done todo', 1);
+insert into todos (text, done) values ('Undone todo', 0);
